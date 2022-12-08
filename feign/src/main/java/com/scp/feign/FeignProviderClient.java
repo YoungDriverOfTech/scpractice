8843e367b8f2aa4e413569a91a4844f3397ce35e
liangxiaole <xiaoleliang045@gmail.com>
2022-12-08 22:56:05 +0900

    Feign

---
+++ feign/src/main/java/com/scp/feign/FeignProviderClient.java
@@ -0,0 +1,20 @@
+package com.scp.feign;
+
+import com.scp.entity.Student;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+// 访问注册中心中注册的provider服务
+@FeignClient(value = "provider")
+public interface FeignProviderClient {
+
+    // 访问微服务里面的方法，不用写实现
+    @GetMapping("/student/findAll")
+    public Collection<Student> findAll();
+
+    // 访问微服务里面的方法，不用写实现
+    @GetMapping("/student/index")
+    public String index();
+}
